70554523a818d9d98b340244327289e959170793
unknown <navdeepthakur2002@gmail.com>
2025-11-01 23:57:18 +0530

    binary search

--- allCodeHere/binarySearch/index.js
+++ allCodeHere/binarySearch/index.js
@@ -1,5 +1,52 @@
+//=----------------------------Binary search====----------------------//
 
+// function binarySearch(arr,target){
+  
+//      if(arr.length<=1){
+//          if(arr[0]==target){
+//             return console.log(0)
+//          }else{
+//             return console.log("Not found")
+//      }
+//     }
+//       let mid = Math.floor(arr.length/2);
 
+//       if(arr[mid]==target){
+//         return console.log(mid)
+//       }else if(arr[mid] > target){
+//         binarySearch(arr.slice(0,mid),target)
+//       }else{
+//         binarySearch(arr.slice(mid),target)
+//       }
+    
+// }
+
+function binarySearch(arr,target){
+    let left=0;
+    let right =arr.length-1;
+   console.log("je;;")
+    while(left<=right){
+        let mid = Math.floor(left+right/2);
+
+        if(arr[mid]==target){
+            return console.log(mid)
+        }else if(arr[mid]<target){
+            left = mid;
+        }else{
+            right = mid;
+        }
+    }
+
+    console.log(-1)
+}
+
+binarySearch([2,3,4,5,6,7,77,88,999],1)
+binarySearch([2,3,4,5,6,7,77,88,999],88)
+binarySearch([2,3,4,5,6,7,77,88,999],6)
+binarySearch([2,3,4,5,6,7,77,88,999],999)
+binarySearch([2],1);
+binarySearch([],1)
+ 
 
 
 
